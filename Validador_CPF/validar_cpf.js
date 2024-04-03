826abd964a8b50f2ebab09aeba770d158374fff1
YuriVascsH <ficryjv@gmail.com>
2024-04-02 23:23:26 -0300

    Adição de laço de repetição para multiplicar os números do cpf.

--- Validador_CPF/validar_cpf.js
+++ Validador_CPF/validar_cpf.js
@@ -1,1 +1,18 @@
-console.log("Olá mundo!");
+//CPF para ser analisado 746.824.890-70
+console.log("-----VALIDADOR CPF-----");
+
+let i = 10;
+let numeros = [];
+let number_string = prompt(
+  "Digite os 9 primeiros dígitos de seu CPF:(Sem caractreres especiais):"
+);
+
+let no = number_string.split("");
+
+for (let indice = 0; indice < number_string; indice++) {
+  while (i > 2) {
+    numeros.push(no[indice] * i);
+    i--;
+  }
+}
+console.log(numeros);
