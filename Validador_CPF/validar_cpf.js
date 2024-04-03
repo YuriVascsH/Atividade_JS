9aec83f49a2e21ef9b91e534dda3b4d6b0e1295d
Yuri Vasconcelos <161763328+YuriVascsH@users.noreply.github.com>
2024-04-03 10:21:52 -0300

    Alterações feitas no laço de repetição
    Retirada do laço de repetição Wihle.

--- Validador_CPF/validar_cpf.js
+++ Validador_CPF/validar_cpf.js
@@ -1,18 +1,19 @@
-//CPF para ser analisado 746.824.890-70
+//CPF para ser analisado 746.824.890-70:
+// 70, 36, 48, 56, 12, 20, 32, 27, 0
+
 console.log("-----VALIDADOR CPF-----");
 
-let i = 10;
+let multiplicador = 10;
 let numeros = [];
+//Adicionar CPF:
 let number_string = prompt(
   "Digite os 9 primeiros dígitos de seu CPF:(Sem caractreres especiais):"
 );
-
-let no = number_string.split("");
-
-for (let indice = 0; indice < number_string; indice++) {
-  while (i > 2) {
-    numeros.push(no[indice] * i);
-    i--;
+let number_solto = number_string.split("");
+console.log(number_solto);
+//Multiplicação dos números
+for (let indice = 0; indice < number_string.length; indice++) {
+      numeros.push(number_solto[indice] * multiplicador);
+      multiplicador--;
   }
-}
-console.log(numeros);
+  console.log(numeros);
