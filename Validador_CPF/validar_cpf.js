25f8cce1e2e0d9f613ad1b7fdfbda8cac787b716
Yuri Vasconcelos <161763328+YuriVascsH@users.noreply.github.com>
2024-04-04 18:39:57 -0300

    Finalizado

--- Validador_CPF/validar_cpf.js
+++ Validador_CPF/validar_cpf.js
@@ -47,3 +47,61 @@ for (let indice = 0; indice < number_solto.length; indice++) {
   segundo_multiplicador--;
 }
 console.log(novos_numeros)
+
+
+//Nova idéia:
+
+console.log('-----VALIDAR CPF-----');
+//Declaração:
+let result = 0;
+let multiplicador = 10;
+let segundo_multiplicador = 11;
+let numeros = [];
+
+//Adicionar CPF:
+let number_string = prompt(
+  "Digite os 9 primeiros dígitos de seu CPF:(Sem caractreres especiais):"
+);
+let number_solto = number_string.split("");
+
+//Multiplicação dos números:
+for (let indice = 0; indice < number_string.length; indice++) {
+  numeros.push(number_solto[indice] * multiplicador);
+  multiplicador--;
+}
+
+for (const numero of numeros) {
+  result += numero;
+}
+result *= 10;
+let restante = result % 11;
+
+//Resultado:
+if (restante > 9) {
+  number_solto.push(0);
+  console.log(`O primeiro dígito de seu CPF é: 0`);
+} else {
+  number_solto.push(restante);
+  console.log(`O primeiro dígito de seu CPF é: ${restante}`);
+}
+
+//Segundo dígito:
+let novos_numeros = [];
+for (let indice = 0; indice < number_solto.length; indice++) {
+  novos_numeros.push(number_solto[indice] * segundo_multiplicador);
+  segundo_multiplicador--;
+}
+
+let soma = novos_numeros.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+let segundo_resto = (soma * 10) % 11;
+
+// Resultado do segundo dígito:
+if (segundo_resto > 9) {
+  number_solto.push(0);
+  console.log(`O segundo dígito de seu CPF é: 0`);
+} else {
+  number_solto.push(segundo_resto);
+  console.log(`O segundo dígito de seu CPF é: ${segundo_resto}`);
+}
+
+console.log(number_solto);
