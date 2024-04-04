5428eddeb03a579f7967d41789c445d139e37c6b
Yuri Vasconcelos <161763328+YuriVascsH@users.noreply.github.com>
2024-04-04 11:21:42 -0300

    Novo número Cpf
    Adição do nova lógica para gerar o último dígito do cpf.

--- Validador_CPF/validar_cpf.js
+++ Validador_CPF/validar_cpf.js
@@ -1,17 +1,21 @@
 //CPF para ser analisado 746.824.890-70:
 // 70, 36, 48, 56, 12, 20, 32, 27, 0
-//1º Dígito do cpf
-console.log("-----VALIDADOR CPF-----");
+// 77, 40, 54, 64, 24, 40, 36, 0, 14
+console.log("-----VALIDAR CPF-----");
+//Declaração:
 let result = 0;
 let multiplicador = 10;
+let segundo_multiplicador = 11;
 let numeros = [];
+
 //Adicionar CPF:
 let number_string = prompt(
   "Digite os 9 primeiros dígitos de seu CPF:(Sem caractreres especiais):"
 );
 let number_solto = number_string.split("");
 console.log(number_solto);
-//Multiplicação dos números
+
+//Multiplicação dos números:
 for (let indice = 0; indice < number_string.length; indice++) {
   numeros.push(number_solto[indice] * multiplicador);
   multiplicador--;
@@ -23,13 +27,23 @@ for (const numero of numeros) {
 result *= 10;
 restante = result % 11;
 console.log(restante);
-
+//Resultado:
 if (restante > 9) {
-  numero.push(0);
+  number_solto.push(0);
   console.log(`O primeiro dígito de seu CPF é: 0`);
 } else {
-  numeros.push(restante);
+  number_solto.push(restante);
   console.log(`O primeto digito de seu CPF é: ${restante}`);
 }
 
-console.log(number_string);
+console.log(number_solto)
+
+//Segundo dígito:
+// Alterar a questão do array:
+
+let novos_numeros = [];
+for (let indice = 0; indice < number_solto.length; indice++) {
+  novos_numeros.push(number_solto[indice] * segundo_multiplicador);
+  segundo_multiplicador--;
+}
+console.log(novos_numeros)
