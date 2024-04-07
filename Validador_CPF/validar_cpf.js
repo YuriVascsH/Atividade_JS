b927db4045c94453f479bc25d325a8626a344a14
Yuri Vasconcelos <161763328+YuriVascsH@users.noreply.github.com>
2024-04-06 21:25:19 -0300

    Código_atualizado

--- Validador_CPF/validar_cpf.js
+++ Validador_CPF/validar_cpf.js
@@ -13,72 +13,22 @@ let number_string = prompt(
   "Digite os 9 primeiros dígitos de seu CPF:(Sem caractreres especiais):"
 );
 let number_solto = number_string.split("");
-console.log(number_solto);
-
-//Multiplicação dos números:
-for (let indice = 0; indice < number_string.length; indice++) {
-  numeros.push(number_solto[indice] * multiplicador);
-  multiplicador--;
-}
-console.log(numeros);
-for (const numero of numeros) {
-  result += numero;
-}
-result *= 10;
-restante = result % 11;
-console.log(restante);
-//Resultado:
-if (restante > 9) {
-  number_solto.push(0);
-  console.log(`O primeiro dígito de seu CPF é: 0`);
-} else {
-  number_solto.push(restante);
-  console.log(`O primeto digito de seu CPF é: ${restante}`);
-}
-
-console.log(number_solto)
-
-//Segundo dígito:
-// Alterar a questão do array:
-
-let novos_numeros = [];
-for (let indice = 0; indice < number_solto.length; indice++) {
-  novos_numeros.push(number_solto[indice] * segundo_multiplicador);
-  segundo_multiplicador--;
-}
-console.log(novos_numeros)
-
-
-//Nova idéia:
-
-console.log('-----VALIDAR CPF-----');
-
-//Declaração:
-let result = 0;
-let multiplicador = 10;
-let segundo_multiplicador = 11;
-let numeros = [];
-
-//Adicionar CPF:
-let number_string = prompt(
-  "Digite os 9 primeiros dígitos de seu CPF:(Sem caractreres especiais):"
-);
-let number_solto = number_string.split("");
 
 //Multiplicação dos números:
 number_string.split("").forEach((value, index) => {
   numeros.push(value * multiplicador);
   multiplicador--;
 });
-
-numeros.forEach(numero => {
+//Calculo:
+numeros.forEach((numero) => {
   result += numero;
 });
 result *= 10;
+
 let restante = result % 11;
 
-//Resultado:
-const addZeroOrRestante = restante => {
+//Resultado do primeiro dígito:
+const addZeroOuRestante = (restante) => {
   if (restante > 9) {
     number_solto.push(0);
     console.log(`O primeiro dígito de seu CPF é: 0`);
@@ -88,7 +38,7 @@ const addZeroOrRestante = restante => {
   }
 };
 
-addZeroOrRestante(restante);
+addZeroOuRestante(restante);
 
 //Segundo dígito:
 let novos_numeros = [];
@@ -97,11 +47,14 @@ number_solto.forEach((value, index) => {
   segundo_multiplicador--;
 });
 
-let soma = novos_numeros.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+let soma = novos_numeros.reduce(
+  (acumulador, valorAtual) => acumulador + valorAtual,
+  0
+);
 let segundo_resto = (soma * 10) % 11;
 
 // Resultado do segundo dígito:
-const addZeroOrSegundoResto = segundo_resto => {
+const addZeroOuSegundoResto = (segundo_resto) => {
   if (segundo_resto > 9) {
     number_solto.push(0);
     console.log(`O segundo dígito de seu CPF é: 0`);
@@ -111,6 +64,6 @@ const addZeroOrSegundoResto = segundo_resto => {
   }
 };
 
-addZeroOrSegundoResto(segundo_resto);
+addZeroOuSegundoResto(segundo_resto);
 
-console.log(number_solto);
+console.log(`O seu cpf é ${number_solto}`);
